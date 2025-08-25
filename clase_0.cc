4999ec562ea71841f727bfe6ef45ddd2332a43b9
flaviooozzz <00115625@uca.edu.sv>
2025-08-25 11:12:08 -0600

    a

--- clase_0.cc
+++ clase_0.cc
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <cmath>
+/*
+declaracion de varaibles constantes
+afuera de toda funcion, 
+const double kpi = 3.14
+osea primero
+const - tipo de dato - k al inicio y luego el nombre de la varaible en snakecase - valor de la variable
+*/
+float CalcularAreaCirculo(int RadioCirculo);
 
 int main(){
-
-    std::cout<<"Hola mundo";
+    float RadioCirculo;
+    std::cout<<"Ingrese el radio de su circulo: "<<std::endl;
+    std::cin>>RadioCirculo;
+    std::cout<<"El area es: "<<round(CalcularAreaCirculo(RadioCirculo));
 
     return 0;
+}
+
+float CalcularAreaCirculo(int RadioCirculo){
+    float AreaCalculadaCirculo=RadioCirculo*RadioCirculo*M_PI;
+    return AreaCalculadaCirculo;
 }
