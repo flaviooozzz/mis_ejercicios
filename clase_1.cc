81c1d18aba6477dee69bca4ba3612b408a3ad575
flaviooozzz <00115625@uca.edu.sv>
2025-08-25 12:40:36 -0600

    fok

--- clase_1.cc
+++ clase_1.cc
@@ -2,28 +2,55 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <utility>
+
+struct InformacionCupon{
+    int cant_cupones;
+    std::string nombre_persona;
+    //std::string cupones_generados[3][3];
+    std::vector<std::pair<std::string,std::string>>cupones_generadors;
+    
+
+}cupon;
 
 std::string GenerarCupon(std::string parametro);
-void AsignarPremio(std::string identificador_cupon);
+std::string AsignarPremio(std::string identificador_cupon);
 std::string SolicitarDatos();
 
 int main(){
-    srand(time(0));  
+    srand(time(0));
+    std::string nombre_persona;
+    std::cout<<"Ingrese su nombre: ";
+    std::cin>>cupon.nombre_persona;
     std::cout<<"Cuantos cupones desea generar: ";
-    int cantidad_de_cupones;
-    std::cin>>cantidad_de_cupones;
-    std::string cupones[cantidad_de_cupones];
-    for (int i=0;i<cantidad_de_cupones;i++){
+    std::cin>>cupon.cant_cupones;
+    std::string cupon_generado;
+    std::string premio;
+    //std::string cupones[cantidad_de_cupones];
+    for (int i=0;i<cupon.cant_cupones;i++){
     
         std::string prefijo = SolicitarDatos();
-        cupones[i] = GenerarCupon(prefijo);
-        std::cout << "Su cupon es: " << cupones[i]<< std::endl;
-
-        AsignarPremio(cupones[i]);
+        cupon_generado=GenerarCupon(prefijo);
+        premio = AsignarPremio(cupon_generado);
+        //cupones[i] = GenerarCupon(prefijo);
+        cupon.cupones_generadors.push_back({cupon_generado,premio});
+        std::cout << "Su cupon es: " << cupon_generado<< std::endl;
     }
-    std::cout<<"Todos sus cupones son: "<<std::endl;
+    /*std::cout<<"\nTodos sus cupones son: "<<std::endl;
     for (int i=0;i<cantidad_de_cupones;i++){
-        std::cout << "Cupon "<<i+1<<": " << cupones[i]<< std::endl;
+        std::cout << "Cupon "<<i+1<<": " << cupones_vector[i]<< std::endl;
+    }*/
+   //se declara la varaible i en auto ya que se "desconoce" el tipo de dato, y recorre posicion por posicion el arreglo,
+   //vector, etc
+    std::cout<<"---Resumen---"<<std::endl;
+    std::cout<< "Nombre: " << cupon.nombre_persona << std::endl;
+    std::cout<< "Cantidad de cupones: " << cupon.cant_cupones << std::endl;   
+    std::cout<<"Todos los cupones: "<<std::endl;
+    for(int i=0;i<cupon.cant_cupones;i++){
+        std::cout<<"Cupon: ";
+        std::cout<<cupon.cupones_generadors[i].first<<" ";
+        std::cout<<cupon.cupones_generadors[i].second<<std::endl;
     }
     return 0;
 }
@@ -34,15 +61,15 @@ std::string GenerarCupon(std::string parametro){
     return parametro + conversion;
 }
 
-void AsignarPremio(std::string identificador_cupon){
+std::string AsignarPremio(std::string identificador_cupon){
     std::string valor_cupon = identificador_cupon.substr(3,3);
     int cupon_numeros = std::stoi(valor_cupon);
 
     if(cupon_numeros % 2 == 0){
-        std::cout << "Tiene premio";
+        return "Tiene premio";
     }
     else{
-        std::cout << "NO tiene premio";
+        return "NO tiene premio";
     }
 }
 
